e2c7fd8157ffde109ecf1d96bb8182439fc3ad31
HysenStublla <45173150+HysenStublla@users.noreply.github.com>
2025-02-25 19:00:12 +0100

    repeated some javascript logic and functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,52 @@
-let emri = "Filan Fisteku";
+let emri = "Filan Fisteku"; //string
+const mosha = 20; //integjer
+const isMarried = true; //boolean
+const ska = null; //Null
+const paDefinuar = undefined; //undefined
+
+const objekti = {
+  emri: "John Doe",
+  mosha: 25,
+  isMarried: true,
+}; //Objekt
+
+const vargu = [1, 2, 3, 4, 5]; //vargu
+
+const objVarg = [
+  {
+    emri: "John Doe",
+    mosha: 25,
+    isMarried: true,
+    student: true,
+  },
+  {
+    emri: "John Doe",
+    mosha: 25,
+    isMarried: true,
+    student: true,
+  },
+  {
+    emri: "John Doe",
+    mosha: 25,
+    isMarried: true,
+  },
+  {
+    emri: "John Doe",
+    mosha: 25,
+    isMarried: true,
+  },
+]; //varg me objekte
+
+console.log(objVarg);
+
+let filterVarObjekt = objVarg.filter(function (elementi) {
+  return elementi.student;
+});
+
+console.log(filterVarObjekt, "Filtered");
+
+let objVariRi = objVarg.map(function (elementi) {
+  return elementi.emri;
+});
+
+console.log(objVariRi);
